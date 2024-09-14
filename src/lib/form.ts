fc9138649d4650dae5926bc1f5c17311f25312aa
Leonardo Baldin <leobaldin.2000@gmail.com>
2024-09-14 03:31:28 +0200

    chore: moved frontend to subfolder

--- src/lib/form.ts
+++
@@ -1,9 +0,0 @@
-export const validateMinLength = (length: number, required = true) => {
-  return {
-    required: required ? "This field is required" : false,
-    minLength: {
-      value: length,
-      message: `The inimum length for this field is ${length}`,
-    },
-  }
-}
